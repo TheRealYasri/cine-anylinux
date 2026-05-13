c22b4a8906f273ec75df4b991340bad7a6304ef1
TheRealYasri <myoung1985@gmail.com>
2026-05-13 12:20:55 -0500

    Initial commit

---
+++ make-appimage.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -eu
+
+# Setup
+VERSION=$(grep -m 1 "version:" Cine/meson.build | cut -d"'" -f2)
+export VERSION
+export ARCH=$(uname -m)
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.hook"
+export ICON=/usr/share/icons/hicolor/scalable/apps/io.github.diegopvlk.Cine.svg
+export DESKTOP=/usr/share/applications/io.github.diegopvlk.Cine.desktop
+export PATH_MAPPING='/usr/share/cine:${SHARUN_DIR}/share/cine'
+export DEPLOY_PYTHON=1
+
+# Deploy dependencies
+quick-sharun \
+    /usr/bin/cine \
+    /usr/lib/libgtk-4.so \
+    /usr/lib/libadwaita-1.so \
+    /usr/lib/libmpv.so.2
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
